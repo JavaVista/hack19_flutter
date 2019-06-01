60e8466c722d5ec75aff2543b0954118703055e7
Joe Glass <joemglass@me.com>
2019-06-01 14:38:18 -0400

    page route update

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,47 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle),
         ),
-        body: MyCustomForm(),
+        body: FirstRoute(),
       ),
-      routes: {'/list': (context) => ListView()},
+    );
+  }
+}
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: <Widget>[MyCustomForm(),RaisedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),],
+      ),
     );
   }
 }
 
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
 // Create a Form Widget
 class MyCustomForm extends StatefulWidget {
   @override
@@ -28,6 +62,7 @@ class MyCustomForm extends StatefulWidget {
     return MyCustomFormState();
   }
 }
+
 
 // Create a corresponding State class. This class will hold the data related to
 // the form.
@@ -69,17 +104,9 @@ class MyCustomFormState extends State<MyCustomForm> {
               child: Text('Submit'),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/list');
-              },
-              child: Text('avail'),
-            ),
-          ),
         ],
       ),
     );
+    
   }
 }
